947234b7854b61c5ef2294ca16415ffbb153d53c
liuji <ji.liu@transwarp.io>
2023-09-11 18:16:14 +0800

    feat: 配置路由和路径别名

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// 添加对webpack的自定义配置
+const path = require("path")
+
+module.exports = {
+  webpack: {
+    // 配置路径别名
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from'@/pages/Login'
+import Layout from "@/pages/Layout";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Layout/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
